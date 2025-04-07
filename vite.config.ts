d51743355ccfe93d35fd6974655673a874e8b3f7
Egor Didenko <didenk.egor@gmail.com>
2025-04-07 16:46:41 +0400

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://egordidenko.github.io/example-grid",
+  base: "/example-grid/",
   plugins: [react()],
 });
