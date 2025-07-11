3e68487f2df1481484981610df36389e1b4783b2
Egor Didenko <didenk.egor@gmail.com>
2025-07-11 10:27:42 +0400

    feat: added ai uploader

--- src/App.tsx
+++ src/App.tsx
@@ -52,75 +52,47 @@ function App() {
         </section>
 
         <section>
-          <div className="row">
+          <div className='row'>
             <h3>List</h3>
-            <label className="switch">
-              <input type="checkbox" onChange={handleFilesViewMode} />
-              <span className="slider round"></span>
+            <label className='switch'>
+              <input type='checkbox' onChange={handleFilesViewMode} />
+              <span className='slider round'></span>
             </label>
             <h3>Grid</h3>
           </div>
         </section>
 
-        <section className="uploader-grid-col-5">
-          <h2>File uploader minimal</h2>
-          <h3>5-column grid</h3>
-          <uc-config
-            filesViewMode={switcher}
-            ctx-name="my-uploader"
-            pubkey="08561eaebb1c3bd9b12f"
-          ></uc-config>
-          <uc-file-uploader-minimal ctx-name="my-uploader"></uc-file-uploader-minimal>
-        </section>
-
-        <section className="uploader-a-r-16-9">
+        <section className='uploader-a-r-16-9'>
           <h2>File uploader minimal</h2>
           <h3>3-column grid with a 16:9 aspect ratio</h3>
           <uc-config
             filesViewMode={switcher}
-            ctx-name="my-uploader-1"
-            pubkey="08561eaebb1c3bd9b12f"
+            ctx-name='my-uploader-1'
+            pubkey='aadfab151dfea016a1b0'
           ></uc-config>
-          <uc-file-uploader-minimal ctx-name="my-uploader-1"></uc-file-uploader-minimal>
+          <uc-file-uploader-minimal ctx-name='my-uploader-1'></uc-file-uploader-minimal>
         </section>
 
-        <section>
-          <h2>File uploader minimal</h2>
-          <h3>3-column grid with a gridShowFilesNames</h3>
-          <uc-config
-            filesViewMode={switcher}
-            gridShowFileNames="true"
-            ctx-name="uploader-grid-show-file-names"
-            pubkey="08561eaebb1c3bd9b12f"
-          ></uc-config>
-          <uc-file-uploader-minimal ctx-name="uploader-grid-show-file-names"></uc-file-uploader-minimal>
-        </section>
-
-        <section className="single-uploader">
+        <section className='single-uploader'>
           <h2>File uploader minimal</h2>
           <h3>Single upload</h3>
           <uc-config
             filesViewMode={switcher}
-            multiple="false"
-            ctx-name="single-uploader"
-            pubkey="08561eaebb1c3bd9b12f"
+            multiple='false'
+            ctx-name='single-uploader'
+            pubkey='aadfab151dfea016a1b0'
           ></uc-config>
-          <uc-file-uploader-minimal ctx-name="single-uploader"></uc-file-uploader-minimal>
+          <uc-file-uploader-minimal ctx-name='single-uploader'></uc-file-uploader-minimal>
         </section>
 
-        <section className="single-uploader">
-          <h2>File uploader minimal</h2>
-          <h3>
-            Single upload and <pre>sourceList="local"</pre>
-          </h3>
+        <section className='single-uploader'>
+          <h2>File uploader regular</h2>
           <uc-config
             filesViewMode={switcher}
-            sourceList="local"
-            multiple="false"
-            ctx-name="single-uploader-local"
-            pubkey="08561eaebb1c3bd9b12f"
+            ctx-name='uploader-regular'
+            pubkey='aadfab151dfea016a1b0'
           ></uc-config>
-          <uc-file-uploader-minimal ctx-name="single-uploader-local"></uc-file-uploader-minimal>
+          <uc-file-uploader-regular ctx-name='uploader-regular'></uc-file-uploader-regular>
         </section>
       </main>
     </Fragment>
