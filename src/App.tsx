010d3ae66d5c8b134bbd79d58682c32c79f1d9bd
Egor Didenko <didenk.egor@gmail.com>
2025-08-27 10:59:45 -0400

    feat: changed localeDefinitionOverride for done button

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,31 @@ import "./App.css";
 
 import * as UC from "@uploadcare/file-uploader";
 import "@uploadcare/file-uploader/web/uc-file-uploader-minimal.min.css";
+import { useEffect, useRef } from "react";
 
 UC.defineComponents(UC);
 
 function App() {
+  const configRef = useRef(null);
+  useEffect(() => {
+    if (configRef?.current) return;
+
+    configRef?.current?.localeDefinitionOverride = {
+      en: {
+        done: "Upload",
+      },
+    };
+  }, []);
+
   return (
     <main>
       <section className='uploader-a-r-1-1'>
         <uc-config
+          ref={configRef}
           filesViewMode='grid'
           ctx-name='my-uploader-1'
           pubkey='aadfab151dfea016a1b0'
+          useCloudImageEditor={false}
         ></uc-config>
         <uc-file-uploader-minimal ctx-name='my-uploader-1'></uc-file-uploader-minimal>
       </section>
